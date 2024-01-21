a598ef436180984bf2752937404fe655a961003a
Afridi <157195242+Afridi404@users.noreply.github.com>
2024-01-21 12:14:02 +0500

    Update Gmail.py

--- Gmail.py
+++ Gmail.py
@@ -114,7 +114,7 @@ logo=(f"""\033[1;37m
 \033[1;32m  • \033[1;37m Tool Owner :-  Afridi-King
 \033[1;32m  • \033[1;37m GitHub     :-  Afridi404
 \033[1;32m  • \033[1;37m Type       :-  Paid
-\033[1;32m  • \033[1;37m Version    :-  V3.4
+\033[1;32m  • \033[1;37m Version    :-  1.0
 os.system('clear')
 #print("\033[1;32m [•]\033[1;37m Join My FaceBook Group...! \033[1;32mThnx");time.sleep(2);os.system('xdg-open https://facebook.com/groups/1245912839659325/')
 
